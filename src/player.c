4ab2d1f3a8aa0faa635d0612fa5ca78ddade7f7e
KdauntlN <henryknzed@gmail.com>
2025-09-15 20:24:08 +1000

    Added ground detection and jumping

--- src/player.c
+++ src/player.c
@@ -1,6 +1,10 @@
 #include "raylib.h"
 #include "player.h"
 
+const float GRAVITY = 0.8;
+int screen_height = 600;
+int distance_to_ground;
+
 // Initialises player variables and data
 void InitPlayer(Player *player, int startX, int startY, int width, int height, int speed) {
     player->x = startX;
@@ -15,7 +19,7 @@ void InitPlayer(Player *player, int startX, int startY, int width, int height, i
     player->speed = speed;
 
     player->facingRight = true;
-    player->onGround = true;
+    player->onGround = false;
 }
 
 // Updates player position and data
@@ -23,9 +27,29 @@ void UpdatePlayer(Player *player) {
     // Checks if the left or right keys are down and updates velocity accordingly
     if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) player->dx -= player->speed * GetFrameTime();
     if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) player->dx += player->speed * GetFrameTime();
+    
+    // Functionality for jumping
+    if ((IsKeyDown(KEY_W) || IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_UP)) && player->onGround) {
+        player->onGround = false;
+        player->dy = -17;
+    }
 
     // Update player position based on velocity
     player->x += player->dx;
+
+    // If the player isn't on the ground, apply gravity until it is
+    if (!player->onGround) {
+        player->dy += GRAVITY;
+        player->y += player->dy;
+
+        // Check if player is about to go below ground
+        if (player->y + player->dy > GetScreenHeight() - player->height) {
+            player->dy = 0;
+            distance_to_ground = GetScreenHeight() - (player->y + player->height);
+            player->y += distance_to_ground + 1;
+            player->onGround = true;
+        }
+    }
 
     // Dampen velocity to slow down player
     player->dx *= 0.97;
